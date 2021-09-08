ccb03657e79035469b6665f929b9a258e95a7a2e
Tancre <outisisout@gmail.com>
2021-09-08 12:49:10 +0200

    push to db + query db

--- geolocsAPI/index.js
+++ geolocsAPI/index.js
@@ -1,34 +1,50 @@
+const { response } = require('express');
 const express = require('express');
+const Datastore = require('nedb');
+
 const app = express();
-const fs = require('fs'); 
+//const fs = require('fs'); 
 
 const port = 3000;
 app.listen(port, () => console.log('listening at ' + port));
 app.use(express.static('public')); 
 app.use(express.json({ limit: '1mb' }));
 
-let saveGeolocs = [];
-let counter = 0;
+const database = new Datastore('database.db');
+database.loadDatabase();
 
-app.post('/api', (req, res) => {
-    console.log('I got a request!');
-    const data = req.body;
-    console.log(req.body);
-    res.json({
-        status: 'success',
-        latitude: data.lat,
-        longitude: data.long
-    });
+//let saveGeolocs = [];
+//let counter = 0;
 
-    console.log('Push to Array');
-    saveGeolocs[counter] = data;
-    counter++
-    console.log(saveGeolocs);
-
-    console.log('Save to file...')
-    const newJSON = JSON.stringify(saveGeolocs);
-    fs.writeFile('geolocs.json', newJSON, function (err) {
-        if (err) throw err;
-            console.log('Saved!');
+app.get('/api', (req, res) => {
+    database.find({}, (err, data) => {
+        if (err) {
+            response.end();
+            return
+        }
+        res.json(data);
     });
 });
+
+app.post('/api', (req, res) => {
+    const data = req.body;
+    const timestamp = Date.now();
+    data.timestamp = timestamp
+    database.insert(data);
+    res.json(data);
+
+    // PUSH TO ARRAY
+    // console.log('Push to Array');
+    // saveGeolocs[counter] = data;
+    // counter++
+    // console.log(saveGeolocs);
+
+    // PUSH TO FILE
+    // console.log('Save to file...')
+    // const newJSON = JSON.stringify(saveGeolocs);
+    // fs.writeFile('geolocs.json', newJSON, function (err) {
+    //     if (err) throw err;
+    //         console.log('Saved!');
+    // });
+});
+
